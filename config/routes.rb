92b8ce7920e9eb523457c10b730972204eded946
mike <mikemcfaddin@gmail.com>
2020-06-08 11:06:49 -0400

    adds user sessions/signup, user and review routes and controller setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,21 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/users" => "users#index"
+    post "/users" => "users#create"
+    get "/users" => "users#show"
+    patch "/users" => "users#update"
+    delete "/users" => "users#delete"
+    post "/sessions" => "sessions#create"
 
-    resources :resources
-    root 'resources#index'
+    post "/reviews" => "reviews#create"
+    get "/reviews" => "reviews#show"
+    patch "/reviews" => "reviews#update"
+    delete "/reviews" => "reviews#delete"
+
+  end
+
+    # resources :resources
+    # root 'resources#index'
 end
