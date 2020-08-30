abd2ae81ed6ed2b38b0e9435b8a4d9807fcec1ec
mike <mikemcfaddin@gmail.com>
2020-08-29 20:26:34 -0400

    added description column to resources along with controller actions and views for description

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_150950) do
+ActiveRecord::Schema.define(version: 2020_08_29_235852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_08_12_150950) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
+    t.text "description"
   end
 
   create_table "reviews", force: :cascade do |t|
