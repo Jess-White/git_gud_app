e41586b0866f112d611486c25f3357abd0fa16f8
mike <mikemcfaddin@gmail.com>
2020-08-31 08:37:52 -0400

    added error message to sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -12,7 +12,7 @@ class Api::SessionsController < ApplicationController
       )
       render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
     else
-      render json: { message: "Unauthorized" }, status: :unauthorized
+      render json: { message: "Unauthorized User" }, status: :unauthorized
     end
   end
 end
