7301bb9c6f0c8985aaaeabe5734b9f8aa02092b0
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-11-07 18:04:06 -0600

    rebuild to redeploy to heroku after fixing some logic on the frontend

---
+++ public/precache-manifest.763ab950a308ffe27216212e44153dc4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afbc5d24d2631f8a2db3cb85e635fe2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91575355f46e0a5610f1",
+    "url": "/static/css/2.840dafd7.chunk.css"
+  },
+  {
+    "revision": "6aa6de19180bfda7e6f6",
+    "url": "/static/css/main.ae540f3f.chunk.css"
+  },
+  {
+    "revision": "91575355f46e0a5610f1",
+    "url": "/static/js/2.c8349e57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c8349e57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aa6de19180bfda7e6f6",
+    "url": "/static/js/main.3d6313f7.chunk.js"
+  },
+  {
+    "revision": "6baf0ce8dc7d566b430e",
+    "url": "/static/js/runtime-main.369504d7.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/static/media/arrow_right.8ef6a08c.svg"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.486c173cebab3b4ce7359eefe7ecc405.js"
+  "/precache-manifest.763ab950a308ffe27216212e44153dc4.js"
 );
 
 self.addEventListener('message', (event) => {
